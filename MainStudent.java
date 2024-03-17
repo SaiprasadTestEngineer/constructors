dc9efe72da609d2ee4f7d35476edd70ffddbb12c
saiprasadTestEngineer <saiprasadpothuganti@gmail>
2024-03-17 11:50:38 +0530

    constructors

---
+++ MainStudent.java
@@ -0,0 +1,39 @@
+package methodConstructor;
+
+public class MainStudent {
+
+	public static void main(String[] args) {
+
+
+
+		/*StudentData st = new StudentData();
+	// 1) using object reference variable initialize the data
+
+	st.sid=101;
+	st.sname="vickky";
+	st.sgrad='A';
+	st.printStudentData();
+		 */
+
+		// 2) using method
+		
+	/* StudentData st = new StudentData();
+	   st.setStudentData(111,"john",'Z');
+	   st.printStudentData();
+	   st.setStudentData(123, "cena", 'c');
+	   st.printStudentData();
+	   st.setStudentData(999,"wwe",'b');
+	   st.printStudentData();
+		*/ 
+
+		// 3) using constructor
+		
+		StudentData st = new StudentData(999,"tim",'X');
+		StudentData st1 = new StudentData(123, "saiprasad", 'p');
+		StudentData st2 = new StudentData(111,"venky",'Z');
+		st.printStudentData();
+		st2.printStudentData();
+		st1.printStudentData();
+	
+	}	
+}
---
+++ StudentData.java
@@ -0,0 +1,29 @@
+package methodConstructor;
+
+public class StudentData {
+
+	int sid;
+	String sname;
+	char sgrad;
+	
+	// below three ways we can store the data into variables
+	
+	void printStudentData(){
+		System.out.println("sid "+"sname "+"sgard");
+		System.out.println("***********************************");
+		System.out.println(sid+" "+sname+" "+sgrad);
+	    System.out.println("sid "+sid+" sname "+sname+" sgrad "+sgrad);
+	}
+	
+	void setStudentData(int id, String name, char grad){
+		sid = id;
+		sname = name;
+		sgrad = grad;
+	}
+	
+	 StudentData(int id, String name, char grad) {
+		sid = id;
+		sname = name;
+		sgrad = grad;
+	}
+}
